46e2b1b9c21ae143b4849e520bc3a260083751bb
박시우 <sw.talk@navercorp.com>
2016-03-30 18:27:41 +0900

    views up

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,25 @@
-from __future__ import unicode_literals
-
 from django.db import models
+#import datetime
+#from django.utils import timezone
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    """
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    """
+
+    def __unicode__(self):              # __str__ on Python 3
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):              # __str__ on Python 3
+        return self.choice_text
+
